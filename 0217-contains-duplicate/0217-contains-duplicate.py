ce12e894d0a1cfd96f4ca290a59cf350f717723a
Satya Pramod <54855529+pramodnsatya@users.noreply.github.com>
2025-07-08 23:24:01 -0400

    Time: 18 ms (63.6%), Space: 26.1 MB (7.15%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        return len(set(nums)) != len(nums)
+        # n = len(nums)
+        # nums.sort()
+        # for i in range(0,n):
+        #     if nums[i] == nums[i-1]:
+        #         return True
+        # return False
+
