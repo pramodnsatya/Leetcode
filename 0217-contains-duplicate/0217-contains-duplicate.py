9883145104433ed8d4aee313cd6764ec2c07c19d
Satyapramod Nagadamudi <54855529+pramodnsatya@users.noreply.github.com>
2025-08-27 14:14:17 -0400

    Update 0217-contains-duplicate.py

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,10 +1,13 @@
 class Solution(object):
     def containsDuplicate(self, nums):
         return len(set(nums)) != len(nums)
-        # n = len(nums)
-        # nums.sort()
-        # for i in range(0,n):
-        #     if nums[i] == nums[i-1]:
-        #         return True
-        # return False
+        ''' 
+        n = len(nums)
+        nums.sort()
+        for i in range(0,n):
+            if nums[i] == nums[i-1]:
+                return True
+        return False
+
+        '''
 
